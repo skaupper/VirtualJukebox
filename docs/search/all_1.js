dd2fae87a53d6bd29dc032756e9715f48639fcb4
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-11-22 11:07:19 +0100

    first version of doxygen docu

---
+++ docs/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['networklistener_1',['NetworkListener',['../classNetworkListener.html',1,'']]],
+  ['networklistenererror_2',['NetworkListenerError',['../classNetworkListenerError.html',1,'']]]
+];
---
+++ docs/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['networklistener_3',['NetworkListener',['../classNetworkListener.html',1,'']]],
+  ['networklistenererror_4',['NetworkListenerError',['../classNetworkListenerError.html',1,'']]]
+];
