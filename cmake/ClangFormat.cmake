b9b04683b294e3011e39e47e3037d1f501347e42
Michael Wurm <wurm.michael95@gmail.com>
2019-11-24 18:20:06 +0100

    add target

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -9,11 +9,17 @@ else()
   message(STATUS "clang-format found: ${CLANG_FORMAT_EXE}")
 endif()
 
-if(CLANG_FORMAT_EXE)
-  add_custom_target(
-          clang-format
-          COMMAND clang-format
-          -style=Google
-          -i ${ALL_SOURCE_FILES}
-  )
-endif()
+# Generate absolute paths
+set(CLANG_FORMAT_SOURCES_RELATIVE ${SOURCES} ${TEST_SOURCES})
+foreach(source ${CLANG_FORMAT_SOURCES_RELATIVE})
+  get_filename_component(source ${source} ABSOLUTE)
+  list(APPEND CLANG_FORMAT_SOURCES_ABS ${source})
+endforeach()
+
+add_custom_target(
+  clang-format
+  COMMAND clang-format
+  -style=Google
+  -i
+  ${CLANG_FORMAT_SOURCES_ABS}
+)
