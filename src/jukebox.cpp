82e8321f907a83a745fae59dc4c99025f85b27fe
Michael Wurm <wurm.michael95@gmail.com>
2019-11-25 11:55:16 +0100

    implement dummy functions

--- src/jukebox.cpp
+++ src/jukebox.cpp
@@ -3,3 +3,56 @@
 // Author      : Michael Wurm <wurm.michael95@gmail.com>
 // Description : Class JukeBox implementation
 //------------------------------------------------------------------------------
+
+#include "jukebox.h"
+
+using namespace std;
+
+variant<string, TError> JukeBox::generateSession() {
+  variant<string, TError> ret;
+
+  // ret = mMem.generateSession();
+
+  return ret;
+}
+
+variant<bool, TError> JukeBox::authenticateAdmin(string password) {
+  variant<bool, TError> ret;
+
+  if (password == mAdminPassword)
+    ret = true;
+
+  return ret;
+}
+
+/*
+variant<vector<Track>, TError> JukeBox::queryMusic(string name) {
+  variant<vector<Track>, TError> ret;
+
+  return ret;
+}
+
+variant<vector<Track>, TError> JukeBox::getCurrentQueue(TQueueType queue) {
+  variant<vector<Track>, TError> ret;
+
+  return ret;
+}
+
+variant<bool, TError> JukeBox::addTrackToQueue(User user, Track track) {
+  variant<bool, TError> ret;
+
+  return ret;
+}
+
+variant<bool, TError> JukeBox::voteTrack(User user, Track track) {
+  variant<bool, TError> ret;
+
+  return ret;
+}
+
+variant<bool, TError> JukeBox::controlPlayer(User user, TPlayerAction action) {
+  variant<bool, TError> ret;
+
+  return ret;
+}
+*/
--- src/jukebox.h
+++ src/jukebox.h
@@ -11,26 +11,29 @@
 #include <variant>
 #include <vector>
 
+//#include "Memory.h"
+//#include "PlayerAction.h"
 //#include "Track.h"
 //#include "User.h"
-//#include "PlayerAction.h"
 
-typedef enum TQueueType { ADMIN, USER };
+typedef std::string TError;
 
 class JukeBox {
  public:
   void start();
 
  private:
-  std::variant<bool, std::string> generateSession();
-  std::variant<bool, std::string> authenticateAdmin(std::string password);
-  std::variant<std::vector<Track>, std::string> queryMusic(std::string name);
-  std::variant<std::vector<Track>, std::string> getCurrentQueue(
-      TQueueType queue);
-  std::variant<bool, std::string> addTrackToQueue(User user, Track track);
-  std::variant<bool, std::string> voteTrack(User user, Track track);
-  std::variant<bool, std::string> controlPlayer(User user,
-                                                TPlayerAction action);
+  std::variant<std::string, TError> generateSession();
+  std::variant<bool, TError> authenticateAdmin(std::string password);
+  std::variant<std::vector<Track>, TError> queryMusic(std::string name);
+  std::variant<std::vector<Track>, TError> getCurrentQueue(TQueueType queue);
+  std::variant<bool, TError> addTrackToQueue(User user, Track track);
+  std::variant<bool, TError> voteTrack(User user, Track track);
+  std::variant<bool, TError> controlPlayer(User user, TPlayerAction action);
+
+  std::string const mAdminPassword = "admin";
+
+  //  Memory mMem;
 };
 
 #endif /* _JUKEBOX_H_ */
