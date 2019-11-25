2d0f4a9d4cb21ff06dfd9bfda454ab5bf7426767
Michael Wurm <wurm.michael95@gmail.com>
2019-11-25 12:06:11 +0100

    instantiate jukebox

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+//------------------------------------------------------------------------------
+// File        : main.cpp
+// Author      : Team Server
+// Description : Main program for project "Virtual JukeBox"
+//-----------------------------------------------------------------
+
 #include <iostream>
 
+#include "jukebox.h"
+
 int main() {
+  JukeBox jukebox;
   std::cout << "Hello world" << std::endl;
   return 0;
 }
