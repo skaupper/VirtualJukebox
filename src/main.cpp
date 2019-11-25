e0d618fec1b08dcdfa8132457d0791be310a2ab5
Michael Wurm <wurm.michael95@gmail.com>
2019-11-25 13:18:32 +0100

    compile and instantiate LoggingProvider

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@
 #include <iostream>
 
 #include "JukeBox.h"
+#include "LoggingProvider.h"
 
 int main() {
   JukeBox jukebox;
+  LoggingProvider log;
+
+  jukebox.start();
+  log.logInfo("Logging the first info message!");
+  log.logError("Logging the first error message!");
+
   std::cout << "Hello world" << std::endl;
   return 0;
 }
