2348adf6e090daab3572f0600ab88277b6738b42
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-11-28 00:48:17 +0100

    update cmake files to search for installations of libmicrohttpd and libhttpserver

--- cmake/LibHttpServer.cmake
+++ cmake/LibHttpServer.cmake
@@ -1,35 +1,33 @@
 # - Find LibHttpServer (https://github.com/etr/libhttpserver/blob/master/cmakemodule/FindLibHttpServer.cmake)
 
 if(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
-    set(LIBHTTPSERVER_FOUND TRUE)
+  set(LIBHTTPSERVER_FOUND TRUE)
+else()
+  find_path(LIBHTTPSERVER_INCLUDE_DIRS httpserverpp
+    /usr/include
+    /usr/include/httpserver
+    /usr/local/include/
+    /usr/local/include/httpserver
+  )
 
-else(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
-    find_path(LIBHTTPSERVER_INCLUDE_DIRS httpserverpp
-      /usr/include
-      /usr/include/httpserver
-      /usr/local/include/
-      /usr/local/include/httpserver
-      )
-
-  find_library(LIBHTTPSERVER_LIBRARIES NAMES httpserver
-      PATHS
-      /usr/lib
-      /usr/local/lib
-      /opt/local/lib
-      )
+  find_library(LIBHTTPSERVER_LIBRARIES httpserver
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+  )
 
   if(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
-      set(LIBHTTPSERVER_FOUND TRUE)
-  else(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
-      set(LIBHTTPSERVER_FOUND FALSE)
-  endif(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
+    set(LIBHTTPSERVER_FOUND TRUE)
+  else()
+    set(LIBHTTPSERVER_FOUND FALSE)
+  endif()
 
   mark_as_advanced(LIBHTTPSERVER_INCLUDE_DIRS LIBHTTPSERVER_LIBRARIES)
+endif()
 
-endif(LIBHTTPSERVER_INCLUDE_DIRS AND LIBHTTPSERVER_LIBRARIES)
-
-
-if(NOT LIBHTTPSERVER_FOUND)
-    message(FATAL_ERROR "libhttpserver not found. Consider using the build script \
-                         with `sudo ../scripts/install_libhttpserver.sh` to install the library on your system")
+if(LIBHTTPSERVER_FOUND)
+  message(STATUS "Found libhttpserver: ${LIBHTTPSERVER_LIBRARIES}")
+else()
+  message(FATAL_ERROR "libhttpserver not found. Consider using the build script \
+                       with `sudo ../scripts/install_libhttpserver.sh` to install the library on your system")
 endif()
