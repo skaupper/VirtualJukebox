8eeebe5c291dc7dbfc320c1ea45472f054d44c01
Stefan <stefan.jahn332@gmail.com>
2019-11-27 16:23:36 +0100

    changed interfaces to avoid hard-copy

--- src/MusicBackend.h
+++ src/MusicBackend.h
@@ -8,19 +8,19 @@
 #define MUSICBACKEND_H_INCLUDED
 
 #include <vector>
-
+#include <memory>
 #include "GlobalTypes.h"
 #include "Queue.h"
 #include "Result.h"
 #include "Track.h"
-#include "Playback.h"
+#include "PlaybackTrack.h"
 
 class MusicBackend{
 public:
     virtual TResultOpt initBackend(void) = 0;
-    virtual TResult<std::vector<Track>> queryTracks(std::string const &pattern, size_t const num) = 0;
-    virtual TResultOpt setPlayback(Track const &track) = 0;
-    virtual TResult<Playback> getCurrentPlayback(void) = 0;
+    virtual TResult<std::vector<std::shared_ptr<Track>>> queryTracks(std::string const &pattern, size_t const num) = 0;
+    virtual TResultOpt setPlayback(std::shared_ptr<Track> const track) = 0;
+    virtual TResult<std::shared_ptr<PlaybackTrack>> getCurrentPlayback(void) = 0;
     virtual TResultOpt pause(void) = 0;
     virtual TResultOpt play() = 0;
     virtual TResult<size_t> getVolume(void) = 0;
