58f8ca2e35ca41c6e03e74bd9f7b6c2c00ae9909
Stefan <stefan.jahn332@gmail.com>
2019-11-27 15:05:36 +0100

    Inital commit of backend sources with dummy implementation

---
+++ src/SpotifyBackend.cpp
@@ -0,0 +1,44 @@
+/** ----------------------------------------------------------------------------
+ * @file    SpotifyBackend.h
+ * @author  Stefan Jahn <stefan.jahn332@gmail.com>
+ * @brief   Class handles Music Playback with a Spotify Backend
+ * ---------------------------------------------------------------------------*/
+
+#include <vector>
+#include "SpotifyBackend.h"
+#include "Track.h"
+
+TResultOpt SpotifyBackend::initBackend() {
+    return std::nullopt;
+}
+
+TResult<std::vector<Track>> SpotifyBackend::queryTracks(std::string const &pattern, size_t const num) {
+    auto tmp = std::vector<Track>();
+    tmp.emplace_back(Track{});
+    return tmp;
+}
+
+TResultOpt SpotifyBackend::setPlayback(Track const &track) {
+    return std::nullopt;
+}
+
+TResult<Playback> SpotifyBackend::getCurrentPlayback() {
+    Track track;
+    return Playback(track);
+}
+
+TResultOpt SpotifyBackend::pause() {
+    return std::nullopt;
+}
+
+TResultOpt SpotifyBackend::play() {
+    return std::nullopt;
+}
+
+TResult<size_t> SpotifyBackend::getVolume() {
+    return 50;
+}
+
+TResultOpt SpotifyBackend::setVolume(size_t const percent) {
+    return Error(ErrorCode::SessionExpired,"Error Message Test");
+}
