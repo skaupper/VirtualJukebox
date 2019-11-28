84f521aae69214059663f746f039ed27b6d864dd
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-11-28 10:27:09 +0100

    update libhttpserver example

--- examples/hello_libhttpserver.cpp
+++ examples/hello_libhttpserver.cpp
@@ -1,12 +1,13 @@
-/** ----------------------------------------------------------------------------
+/**
  * @file    hello_libhttpserver.cpp
  * @author  Sebastian Kaupper <kauppersebastian@gmail.com>
  * @brief   Example program showcasing libhttpserver
  *
- * This program starts a server on port 8080 which listens to the endpoint /hello
- * To verify that it's working use `url -XGET %http://localhost:8080/hello` or use
- * a browser and go to <http://localhost:8080/hello>
- * ---------------------------------------------------------------------------*/
+ * @details This program starts a server on port 8080 which listens to the
+ * endpoint `/hello`. To verify that it's working use `curl -XGET
+ * %http://localhost:8080/hello` or use a browser and go to
+ * http://localhost:8080/hello.
+ */
 
 #include <httpserver.hpp>
 
@@ -14,12 +15,12 @@ using namespace httpserver;
 
 class hello_world_resource : public http_resource {
  public:
-  const std::shared_ptr<http_response> render(const http_request&) {
-    return std::shared_ptr<http_response>(new string_response("Hello, libhttpserver!"));
+  const std::shared_ptr<http_response> render(const http_request &) {
+    return std::make_shared<string_response>("Hello, libhttpserver!");
   }
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
   webserver ws = create_webserver(8080);
 
   hello_world_resource hwr;
