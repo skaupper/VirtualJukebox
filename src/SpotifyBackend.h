083bbca42a8835932b10c7834a652f8f556aba88
Stefan <stefan.jahn332@gmail.com>
2019-11-29 10:49:11 +0100

    clang formatted

--- src/SpotifyBackend.h
+++ src/SpotifyBackend.h
@@ -7,24 +7,24 @@
 #ifndef SPOTIFYBACKEND_H_INCLUDED
 #define SPOTIFYBACKEND_H_INCLUDED
 
-#include "MusicBackend.h"
 #include "GlobalTypes.h"
+#include "MusicBackend.h"
+#include "PlaybackTrack.h"
 #include "Queue.h"
 #include "Result.h"
 #include "Track.h"
-#include "PlaybackTrack.h"
 
 class SpotifyBackend : public MusicBackend {
-public:
-    virtual TResultOpt initBackend(void) override;
-    virtual TResult<std::vector<Track>> queryTracks(std::string const &pattern, size_t const num) override;
-    virtual TResultOpt setPlayback(Track const &track) override;
-    virtual TResult<PlaybackTrack> getCurrentPlayback(void) override;
-    virtual TResultOpt pause(void) override;
-    virtual TResultOpt play() override;
-    virtual TResult<size_t> getVolume(void) override;
-    virtual TResultOpt setVolume(size_t const percent) override;
-
+ public:
+  virtual TResultOpt initBackend(void) override;
+  virtual TResult<std::vector<Track>> queryTracks(std::string const &pattern,
+                                                  size_t const num) override;
+  virtual TResultOpt setPlayback(Track const &track) override;
+  virtual TResult<PlaybackTrack> getCurrentPlayback(void) override;
+  virtual TResultOpt pause(void) override;
+  virtual TResultOpt play() override;
+  virtual TResult<size_t> getVolume(void) override;
+  virtual TResultOpt setVolume(size_t const percent) override;
 };
 
-#endif //SPOTIFYBACKEND_H_INCLUDED
+#endif  // SPOTIFYBACKEND_H_INCLUDED
