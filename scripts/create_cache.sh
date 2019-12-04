09da5349a1ce9ac510c93a3a47a7adefbec3a73c
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-12-04 23:20:53 +0100

    libhttpserver should be cached too

--- scripts/create_cache.sh
+++ scripts/create_cache.sh
@@ -7,6 +7,7 @@ mkdir -p cached_libs/ cached_libs/lib/ cached_libs/include/
 
 # copy all necessary files to the cache
 cp    /usr/local/lib/librestclient-cpp.so cached_libs/lib/
+cp    /usr/local/lib/libhttpserver.so     cached_libs/lib/
 cp -r /usr/local/include/restclient-cpp/  cached_libs/include/
 cp -r /usr/local/include/httpserver/      cached_libs/include/
 cp    /usr/local/include/httpserverpp     cached_libs/include/
