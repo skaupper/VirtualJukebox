03017b23a55f138dcd4dea444218f6c10cf63299
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-12-05 00:35:42 +0100

    Travis: cache libraries to save on build time (#38)

---
+++ scripts/check_cache.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "### Check library cache."
+
+if [ "`ls -A cached_libs/`" ]; then
+    echo "Cache found, do not build libraries."
+    sudo cp -r cached_libs/* /usr/local/;
+else
+    echo "No cache found, build libraries.";
+fi
+
+if [ ! -f /usr/local/lib/libhttpserver.so ]; then
+    echo "### Build libhttpserver."
+    ./scripts/install_libhttpserver.sh;
+fi
+
+if [ ! -f /usr/local/lib/librestclient-cpp.so ]; then
+    echo "### Build librestclient-cpp."
+    ./scripts/install_librestclient-cpp.sh;
+fi
---
+++ scripts/create_cache.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "### Create library cache."
+
+# create cache dir
+mkdir -p cached_libs/lib/ cached_libs/include/
+
+# copy all necessary files to the cache
+cp    /usr/local/lib/librestclient-cpp.so cached_libs/lib/
+cp    /usr/local/lib/libhttpserver.so     cached_libs/lib/
+cp    /usr/local/lib/libhttpserver.so.0   cached_libs/lib/
+cp -r /usr/local/include/restclient-cpp/  cached_libs/include/
+cp -r /usr/local/include/httpserver/      cached_libs/include/
+cp    /usr/local/include/httpserverpp     cached_libs/include/
+cp    /usr/local/include/httpserver.hpp   cached_libs/include/
