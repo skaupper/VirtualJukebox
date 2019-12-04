da0709e4966ae6c99a61d083db9df0289de1b8cc
Sebastian Kaupper <kauppersebastian@gmail.com>
2019-12-04 23:57:37 +0100

    hopefully theses brackets are not necessary

--- scripts/check_cache.sh
+++ scripts/check_cache.sh
@@ -4,19 +4,19 @@ set -euo pipefail
 
 echo "### Check library cache."
 
-if [[ "`ls -A cached_libs/`" ]]; then
+if [ "`ls -A cached_libs/`" ]; then
     echo "Cache found, do not build libraries."
     sudo cp -r cached_libs/* /usr/local/;
 else
     echo "No cache found, build libraries.";
 fi
 
-if [[ ! -f /usr/local/lib/libhttpserver.so ]]; then
+if [ ! -f /usr/local/lib/libhttpserver.so ]; then
     echo "### Build libhttpserver."
     ./scripts/install_libhttpserver.sh;
 fi
 
-if [[ ! -f /usr/local/lib/librestclient-cpp.so ]]; then
+if [ ! -f /usr/local/lib/librestclient-cpp.so ]; then
     echo "### Build librestclient-cpp."
     ./scripts/install_librestclient-cpp.sh;
 fi
