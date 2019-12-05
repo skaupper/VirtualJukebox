816173ff1b8c6ac2f684ced64d4c0c8e052ad619
Michael Wurm <wurm.michael95@gmail.com>
2019-12-05 14:52:20 +0100

    fix rebase

--- src/Network/NetworkAPI.h
+++ src/Network/NetworkAPI.h
@@ -1,34 +1,22 @@
-<<<<<<< HEAD
 /*****************************************************************************/
-=======
->>>>>>> squash for rebase
 /**
  * @file    NetworkAPI.h
  * @author  Sebastian Kaupper <kauppersebastian@gmail.com>
  * @brief   Definition of class NetworkAPI
  */
-<<<<<<< HEAD
 /*****************************************************************************/
 
 #ifndef _NETWORK_API_H_
 #define _NETWORK_API_H_
-=======
-
-#ifndef NETWORK_API_H_INCLUDED
-#define NETWORK_API_H_INCLUDED
->>>>>>> squash for rebase
 
 #include "NetworkListener.h"
 
 /**
  * @class NetworkAPI
  * @brief Base class for all implementations of network interfaces.
-<<<<<<< HEAD
  *
  * @details Creates a webserver and notifies a listener about (valid) incoming
  * requests using the observer pattern.
-=======
->>>>>>> squash for rebase
  */
 class NetworkAPI {
  public:
@@ -42,15 +30,8 @@ class NetworkAPI {
   NetworkAPI(NetworkAPI const &) = delete;
   NetworkAPI &operator=(NetworkAPI const &) = delete;
 
-<<<<<<< HEAD
  protected:
   NetworkListener *listener;
 };
 
 #endif /* _NETWORK_API_H_ */
-=======
-  NetworkListener *listener;
-};
-
-#endif  // NETWORK_API_H_INCLUDED
->>>>>>> squash for rebase
