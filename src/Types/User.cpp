ff0669f87e01ef67c9281efbf6824d8aeecb26dd
Samuel <samuel.hick@gmx..at>
2019-12-13 14:32:28 +0100

    implemented feedback from pull req

--- src/Types/User.cpp
+++
@@ -1,9 +0,0 @@
-/** ----------------------------------------------------------------------------
- * @file    User.cpp
- * @author  Samuel Hick <samuel.hick@gmx.at>
- * @brief   Class User implementation
- * ---------------------------------------------------------------------------*/
-
-#include "User.h"
-
-using namespace std;
